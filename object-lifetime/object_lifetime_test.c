a809917f3678dc616d0354dbab20fb77bb79b426
Robin Voetter <robin@streamhpc.com>
2022-05-05 11:08:09 +0200

    object lifetime tests: reformat

--- object-lifetime/object_lifetime_test.c
+++ object-lifetime/object_lifetime_test.c
@@ -8,15 +8,13 @@
 #include <stdlib.h> // malloc
 #include <stdint.h> // UINTMAX_MAX
 
-void expectSuccess(cl_int err, const char * name)
-{
-    if (err != CL_SUCCESS)
-    {
-        printf("ERROR: %s (%i)\n", name, err);
-        exit( err );
-    } else {
-        printf("SUCCESS: %s\n", name);
-    }
+void expectSuccess(cl_int err, const char * name) {
+  if (err != CL_SUCCESS) {
+    printf("ERROR: %s (%i)\n", name, err);
+    exit( err );
+  } else {
+    printf("SUCCESS: %s\n", name);
+  }
 }
 
 void expectErr(cl_int err, const char *name) {
@@ -29,73 +27,68 @@ void expectErr(cl_int err, const char *name) {
   }
 }
 
-void logError(cl_int err, const char * name)
-{
-    if (err != CL_SUCCESS)
-    {
-        printf("ERROR: %s (%i)\n", name, err);
-    } else {
-        printf("SUCCESS: %s\n", name);
-    }
+void logError(cl_int err, const char * name) {
+  if (err != CL_SUCCESS) {
+    printf("ERROR: %s (%i)\n", name, err);
+  } else {
+    printf("SUCCESS: %s\n", name);
+  }
 }
 
-int main()
-{
-    cl_int CL_err = CL_SUCCESS;
-    cl_uint numPlatforms = 0;
+int main() {
+  cl_int status = CL_SUCCESS;
+  cl_uint numPlatforms = 0;
 
-    cl_platform_id platform;
-    CL_err = clGetPlatformIDs(1, &platform, &numPlatforms);
-    expectSuccess(CL_err, "clGetPlatformIDs");
-    if (numPlatforms == 0)
-    {
-        printf("No OpenCL platform detected.\n");
-        exit(-1);
-    }
-    
-    cl_device_id device;
-    cl_uint      numDevices;
-    CL_err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, &numDevices);
-    expectSuccess(CL_err, "clGetDeviceIDs");
-    if(numDevices == 0) {
-        printf("No OpenCL device found.\n");
-        exit(-1);
-    }
+  cl_platform_id platform;
+  status = clGetPlatformIDs(1, &platform, &numPlatforms);
+  expectSuccess(status, "clGetPlatformIDs");
+  if (numPlatforms == 0) {
+    printf("No OpenCL platform detected.\n");
+    exit(-1);
+  }
 
-    cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)(platform), 0};
+  cl_device_id device;
+  cl_uint      numDevices;
+  status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, &numDevices);
+  expectSuccess(status, "clGetDeviceIDs");
+  if(numDevices == 0) {
+    printf("No OpenCL device found.\n");
+    exit(-1);
+  }
 
-    cl_context context =
-        clCreateContext(properties, 1, &device, NULL, NULL, &CL_err);
-    expectSuccess(CL_err, "clCreateContext");
+  cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)(platform), 0};
 
-    // Create a buffer from the context
-    cl_mem buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, 1, NULL, &CL_err);
-    expectSuccess(CL_err, "clCreateBuffer");
+  cl_context context = clCreateContext(properties, 1, &device, NULL, NULL, &status);
+  expectSuccess(status, "clCreateContext");
 
-    // Release the context, but the buffer should keep it alive
-    CL_err = clReleaseContext(context);
-    expectSuccess(CL_err, "clReleaseContext");
+  // Create a buffer from the context
+  cl_mem buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, 1, NULL, &status);
+  expectSuccess(status, "clCreateBuffer");
 
-    // Use the context
-    cl_uint ref_count = 0;
-    CL_err = clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT,
-                             sizeof(cl_uint), &ref_count, NULL);
-    expectSuccess(CL_err, "clGetContextInfo");
+  // Release the context, but the buffer should keep it alive
+  status = clReleaseContext(context);
+  expectSuccess(status, "clReleaseContext");
 
-    // Release the buffer, this should also release the context
-    CL_err = clReleaseMemObject(buffer);
-    expectSuccess(CL_err, "clReleaseMemObject");
+  // Use the context
+  cl_uint ref_count = 0;
+  status = clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT,
+                            sizeof(cl_uint), &ref_count, NULL);
+  expectSuccess(status, "clGetContextInfo");
 
-    // Use the context, this should fail as the context should be already deleted at this point
-    CL_err = clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT,
-                             sizeof(cl_uint), &ref_count, NULL);
-    logError(CL_err, "clGetContextInfo");
+  // Release the buffer, this should also release the context
+  status = clReleaseMemObject(buffer);
+  expectSuccess(status, "clReleaseMemObject");
 
-    // Try to release the context again
-    CL_err = clReleaseContext(context);
-    logError(CL_err, "clReleaseContext");
+  // Use the context, this should fail as the context should be already deleted at this point
+  status = clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT,
+                            sizeof(cl_uint), &ref_count, NULL);
+  logError(status, "clGetContextInfo");
 
-    fflush(stdout);
+  // Try to release the context again
+  status = clReleaseContext(context);
+  logError(status, "clReleaseContext");
 
-    return 0;
+  fflush(stdout);
+
+  return 0;
 }
