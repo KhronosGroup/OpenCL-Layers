fbe336165949908d35969ff8d64b09768f5a094d
Robin Voetter <robin@streamhpc.com>
2022-05-10 13:06:36 +0200

    object lifetime tests: commandqueue on_device_default edge case

--- object-lifetime/object_lifetime_test.cpp
+++ object-lifetime/object_lifetime_test.cpp
@@ -653,6 +653,36 @@ void testPipeline(cl_platform_id platform, cl_device_id device) {
   EXPECT_DESTROYED(context);
 }
 
+void testCommandQueueEdgeCase(cl_platform_id platform, cl_device_id device) {
+  cl_int status;
+  cl_context context = createContext(platform, device);
+
+  cl_queue_properties props[] = {CL_QUEUE_PROPERTIES, CL_QUEUE_ON_DEVICE | CL_QUEUE_ON_DEVICE_DEFAULT , 0};
+  cl_command_queue queue_a = clCreateCommandQueueWithProperties(context,
+                                                                device,
+                                                                props,
+                                                                &status);
+  EXPECT_SUCCESS(status);
+  EXPECT_REF_COUNT(queue_a, 1, 0);
+
+  // Creating extra queues with CL_QUEUE_ON_DEVICE_DEFAULT will return the same queue and increment its reference count.
+  cl_command_queue queue_b = clCreateCommandQueueWithProperties(context,
+                                                                device,
+                                                                props,
+                                                                &status);
+  EXPECT_SUCCESS(status);
+  EXPECT_REF_COUNT(queue_a, 2, 0);
+  EXPECT_REF_COUNT(queue_b, 2, 0);
+
+  EXPECT_SUCCESS(clReleaseCommandQueue(queue_a));
+  EXPECT_REF_COUNT(queue_a, 1, 0);
+  EXPECT_REF_COUNT(queue_b, 1, 0);
+
+  EXPECT_SUCCESS(clReleaseCommandQueue(queue_b));
+  EXPECT_DESTROYED(queue_a);
+  EXPECT_DESTROYED(queue_b);
+}
+
 int main(int argc, char *argv[]) {
   parseArgs(TEST_CONFIG, argc, argv);
 
@@ -702,6 +732,7 @@ int main(int argc, char *argv[]) {
   testSubObjects(platform, device);
   testSubDevice(platform, device);
   testPipeline(platform, device);
+  testCommandQueueEdgeCase(platform, device); // TODO: This test requires at least OpenCL 2.0
 
   return 0;
 }
