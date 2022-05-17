017ea351c22e6bd883f477210bbbe5ca360f425e
Robin Voetter <robin@streamhpc.com>
2022-05-17 11:10:24 +0200

    object lifetime tests: leak/resurrect test

--- object-lifetime/test/leak.cpp
+++ object-lifetime/test/leak.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
   cl_platform_id platform;
   cl_device_id device;
   cl_int status;
-  layer_test::setup(argc, argv, CL_MAKE_VERSION(3, 0, 0), platform, device);
+  layer_test::setup(argc, argv, CL_MAKE_VERSION(1, 1, 0), platform, device);
 
   cl_context context = layer_test::createContext(platform, device);
 
@@ -12,18 +12,22 @@ int main(int argc, char *argv[]) {
   EXPECT_SUCCESS(status);
   EXPECT_REF_COUNT(buffer, 1, 0);
 
-  EXPECT_SUCCESS(clReleaseContext(context));
-  EXPECT_REF_COUNT(context, 0, 1);
+  cl_sampler sampler = clCreateSampler(context, CL_TRUE, CL_ADDRESS_CLAMP, CL_FILTER_LINEAR, &status);
+  EXPECT_SUCCESS(status);
+  EXPECT_REF_COUNT(sampler, 1, 0);
 
-  // Try to 'resurrect' the context
-  EXPECT_SUCCESS(clRetainContext(context));
-  EXPECT_REF_COUNT(context, 1, 1);
+  cl_event event = clCreateUserEvent(context, &status);
+  EXPECT_SUCCESS(status);
+  EXPECT_REF_COUNT(event, 1, 0);
+
   EXPECT_SUCCESS(clReleaseContext(context));
-  EXPECT_REF_COUNT(context, 0, 1);
+  EXPECT_REF_COUNT(context, 0, 2);
 
   EXPECT_SUCCESS(clReleaseMemObject(buffer));
   EXPECT_DESTROYED(buffer); // recently deleted with type: BUFFER
-  EXPECT_DESTROYED(context); // recently deleted with type: CONTEXT
+  EXPECT_REF_COUNT(context, 0, 1);
+
+  // Leak sampler and event
 
   return layer_test::finalize();
 }
