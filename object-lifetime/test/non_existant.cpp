fb2a6acc51ccf23e2e2c45235f6b887216751049
Robin Voetter <robin@streamhpc.com>
2022-05-19 13:15:26 +0200

    object lifetime test: test error for non-existant object

---
+++ object-lifetime/test/non_existant.cpp
@@ -0,0 +1,17 @@
+#include "object_lifetime_test.hpp"
+
+int main(int argc, char *argv[]) {
+  cl_platform_id platform;
+  cl_device_id device;
+  layer_test::setup(argc, argv, CL_MAKE_VERSION(1, 1, 0), platform, device);
+
+  cl_context context = layer_test::createContext(platform, device);
+
+  cl_mem non_existant_buffer = reinterpret_cast<cl_mem>(0xAAAAAAAA);
+  EXPECT_DESTROYED(non_existant_buffer); // used but does not exist
+
+  EXPECT_SUCCESS(clReleaseContext(context));
+  EXPECT_DESTROYED(context); // recently deleted with type: CONTEXT
+
+  return layer_test::finalize();
+}
