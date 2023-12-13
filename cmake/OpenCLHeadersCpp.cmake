6755ae9c7b928bcbd08d0d82b5ec94db75d1ff1f
Lőrinc Serfőző <lorinc@streamhpc.com>
2023-12-13 09:37:58 +0100

    Workaround not to build OpenCL-CLHPP examples

--- cmake/OpenCLHeadersCpp.cmake
+++ cmake/OpenCLHeadersCpp.cmake
@@ -14,5 +14,20 @@ if (NOT OpenCLHeadersCpp_FOUND)
     GIT_REPOSITORY https://github.com/KhronosGroup/OpenCL-CLHPP.git
     GIT_TAG        v2023.04.17
   )
+
+  # A workaround to disable the building of the examples
+  # which requires the OpenCL-ICD-Loader CMake project to be installed.
+  if(DEFINED CACHE{BUILD_EXAMPLES})
+    set(BUILD_EXAMPLES_CACHE_VAL "${BUILD_EXAMPLES}")
+  endif()
+  set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+
   FetchContent_MakeAvailable(OpenCLHeadersCpp)
+
+  # Restoring the original state
+  if(DEFINED BUILD_EXAMPLES_CACHE_VAL)
+    set(BUILD_EXAMPLES "${BUILD_EXAMPLES_CACHE_VAL}" CACHE BOOL "" FORCE)
+  else()
+    unset(BUILD_EXAMPLES CACHE)
+  endif()
 endif ()
